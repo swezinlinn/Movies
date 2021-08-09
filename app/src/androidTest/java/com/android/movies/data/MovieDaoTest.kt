1069090afd2af6cbc0f4ce16dc58687e7cafd61a
swezinlinn <swezinlinn1@gmail.com>
2021-08-09 21:33:46 +0630

    test modify

--- app/src/androidTest/java/com/android/movies/data/MovieDaoTest.kt
+++ app/src/androidTest/java/com/android/movies/data/MovieDaoTest.kt
@@ -6,6 +6,8 @@ import androidx.test.runner.AndroidJUnit4
 import com.android.movie.util.TestUtil
 import com.android.movies.data.remote.db.AppDatabase
 import com.android.movies.data.remote.db.entity.MovieEntity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.After
 import org.junit.Assert.*
@@ -36,27 +38,36 @@ class MovieDaoTest {
 
     @Test
     @Throws(Exception::class)
-    suspend fun isPhotoListEmpty(){
-        assertEquals(0,mDatabase.movieDao.getSearchedMovieList("Aventure").size)
+    fun isPhotoListEmpty(){
+        val myScope = GlobalScope
+        myScope.launch {
+            assertEquals(0, mDatabase.movieDao.getSearchedMovieList("Aventure").size)
+        }
     }
 
 
     @Test
     @Throws(Exception::class)
-    suspend fun insertPhoto() {
-        val photo: List<MovieEntity> = TestUtil.createMovieList(6)
-        val insertedPhoto = mDatabase.movieDao.insertAll(photo)
-        assertNotNull(insertedPhoto)
+    fun insertMovie() {
+        val myScope = GlobalScope
+        myScope.launch {
+            val photo: List<MovieEntity> = TestUtil.createMovieList(6)
+            val insertedPhoto = mDatabase.movieDao.insertAll(photo)
+            assertNotNull(insertedPhoto)
+        }
     }
 
 
     @Test
     @Throws(Exception::class)
-    suspend fun insertPhotoAndLoadByTitle() {
+    fun insertMovieAndLoadByTitle() {
+        val myScope = GlobalScope
         val photo: List<MovieEntity> = TestUtil.createMovieList(6)
-        mDatabase.movieDao.insertAll(photo)
-        val movieLoadedByName = mDatabase.movieDao.getSearchedMovieList("Aventure")
-        assertEquals(movieLoadedByName, equalTo(photo))
+        myScope.launch {
+            mDatabase.movieDao.insertAll(photo)
+            val movieLoadedByName = mDatabase.movieDao.getSearchedMovieList("Aventure")
+            assertEquals(movieLoadedByName, equalTo(photo))
+        }
     }
 
 }
